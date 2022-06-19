70f29d4d6d2f166c4fc3350f0a778ff1679609b6
griffi-gh <prasol258@gmail.com>
2022-06-19 14:40:44 +0200

    aa

--- src/world.rs
+++ src/world.rs
@@ -7,24 +7,21 @@ impl WorldManager {
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
   ) {
+    let mat_handle = materials.add(StandardMaterial {
+      base_color: Color::rgb(0.3, 0.5, 0.3),
+      unlit: true,
+      ..default()
+    });
     // plane
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(StandardMaterial {
-          base_color: Color::rgb(0.3, 0.5, 0.3),
-          unlit: true,
-          ..default()
-        }),
+        material: mat_handle.clone(),
         ..default()
     });
     // cube
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(StandardMaterial {
-          base_color: Color::rgb(0.8, 0.7, 0.6),
-          unlit: true,
-          ..default()
-        }),
+        material: mat_handle,
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
