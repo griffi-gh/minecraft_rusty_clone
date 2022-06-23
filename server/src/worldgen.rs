9b91b404a089c1fa570b2275e4227e887c25f2e5
griffi-gh <prasol258@gmail.com>
2022-06-24 01:56:51 +0200

    uwu

--- server/src/worldgen.rs
+++ server/src/worldgen.rs
@@ -5,13 +5,27 @@ use shared::{
 use noise::{Fbm, NoiseFn};
 use rand::{rngs::SmallRng, SeedableRng, Rng};
 
-const TERRAIN_H_SCALE:    f64   = 0.04;
-const MIN_TERRAIN_HEIGHT: usize = 85;
-const TERRAIN_HEIGHT: f64       = 35.;
-const CAVE_AMOUNT: f64          = 0.50;   //Range: 0 - 1
-const CAVE_TOP_REDUCE: f64      = 0.16;   //Range: 0 - 1
-const HANGING_TRESHOLD: u8      = 9;
-const PRNG_SEED: u64            = 0x0DDB1A5E5BAD5EED; //Used only for bedrock generation
+//========================================
+
+const TERRAIN_H_SCALE:    f64    = 0.04;
+const MIN_TERRAIN_HEIGHT: usize  = 85;
+const TERRAIN_HEIGHT: f64        = 35.;
+
+const GENERATE_CAVES: bool       = true;
+const USE_OLD_CAVES: bool        = false; //Use old system that's based on noise treshold instead of Perlin worms
+//new system config
+const CAVE_GAP_WIDTH: f64        = 0.15;   //Range: 0 - 1 (Only used by the new system)
+const TOP_CAVE_GAP_REDUCE: f64   = 0.9;    //Range: 0 - 1 (Only used by the new system, percentage of CAVE_GAP_WIDTH)
+//old system_config
+const CAVE_AMOUNT: f64           = 0.50;  //Range: 0 - 1 (Only used by the old system)
+const CAVE_TOP_REDUCE: f64       = 0.16;  //Range: 0 - 1 (Only used by the old system)
+
+const HANGING_CHECK: bool        = false; //Can look pretty weird, so disabled by default
+const HANGING_TRESHOLD: u8       = 6;
+
+const PRNG_SEED: u64             = 0x0DDB1A5E5BAD5EED; //Used only for bedrock generation
+
+//========================================
 
 const TERRAIN_MAX_HEIGHT: f64 = MIN_TERRAIN_HEIGHT as f64 + TERRAIN_HEIGHT;
 const TERRAIN_HEIGHT_HALF: f64 = TERRAIN_HEIGHT / 2.;
@@ -28,7 +42,15 @@ pub fn generate(x: i64, y: i64) -> ChunkData {
   //Create FBM (Fractional Brownian Motion) noise generator
   //WARNING! fbm.get() return data in range -1..=1, NOT 0..=1
   let fbm = Fbm::new();
-  let mut rng = SmallRng::seed_from_u64(PRNG_SEED);
+  let cave_fbm = {
+    let mut fbm = Fbm::new();
+    fbm.octaves = 2;
+    fbm
+  };
+
+
+  //Create RNG
+  let mut rng = SmallRng::seed_from_u64(PRNG_SEED ^ (x as u64) ^ (y as u64));
 
   //Create random
 
@@ -42,18 +64,24 @@ pub fn generate(x: i64, y: i64) -> ChunkData {
       //Generate terrain
       for y in 0..h {
         //Generate caves
-        let cave_check = {
+        let cave_check = if GENERATE_CAVES {
           let point_3d = [x_offset + x as f64, y as f64, y_offset + z as f64].map(|x| x * TERRAIN_H_SCALE);
-          ((fbm.get(point_3d) + 1.) / 2.) >= (CAVE_AMOUNT - (y as f64 / TERRAIN_MAX_HEIGHT).min(1.) * CAVE_TOP_REDUCE)
-        };
+          if USE_OLD_CAVES {
+            ((fbm.get(point_3d) + 1.) / 2.) < (CAVE_AMOUNT - (y as f64 / TERRAIN_MAX_HEIGHT).min(1.) * CAVE_TOP_REDUCE)
+          } else {
+            let gap_width = CAVE_GAP_WIDTH - CAVE_GAP_WIDTH * TOP_CAVE_GAP_REDUCE * (y as f64 / TERRAIN_MAX_HEIGHT).min(1.);
+            ((0.5-(gap_width/2.))..=(0.5+(gap_width/2.))).contains(&((cave_fbm.get(point_3d) + 1.) / 2.))
+          }
+        } else { false };
         //If no cave, place a block
-        if cave_check {
+        if !cave_check {
           blocks[x][y][z] = Block { block_type: 1 };
         }
       }
 
       //"Collapse" gaps
-      {
+      //Disabled by default!
+      if HANGING_CHECK {
         let mut gap: u8 = 0;
         let mut hanging = false;
         for y in MIN_TERRAIN_HEIGHT..h {
