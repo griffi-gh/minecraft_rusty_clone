a34e471642623d8cfb517e5efbef079b576f539e
griffi-gh <prasol258@gmail.com>
2022-06-24 00:19:08 +0200

    fuck

--- server/src/worldgen.rs
+++ server/src/worldgen.rs
@@ -4,11 +4,12 @@ use shared::{
 };
 use noise::{Fbm, NoiseFn};
 
-const TERRAIN_H_SCALE:    f64   = 0.1;
+const TERRAIN_H_SCALE:    f64   = 0.05;
 const MIN_TERRAIN_HEIGHT: usize = 10;
 const MAX_TERRAIN_HEIGHT: f64   = 40.;
 
 const TERRAIN_HEIGHT: f64 = MAX_TERRAIN_HEIGHT - MIN_TERRAIN_HEIGHT as f64;
+const TERRAIN_HEIGHT_HALF: f64 = TERRAIN_HEIGHT / 2.;
 
 pub fn generate(x: i64, y: i64) -> ChunkData {
   //Get X/Y offsets
@@ -20,12 +21,16 @@ pub fn generate(x: i64, y: i64) -> ChunkData {
   let blocks = &mut data.0;
 
   //Create FBM (Fractional Brownian Motion) noise generator
-  let fbm = Fbm::new();
+  let fbm = {
+    let mut fbm = Fbm::new();
+    fbm.octaves = 3;
+    fbm
+  };
 
   for x in 0..CHUNK_SIZE {
     for z in 0..CHUNK_SIZE {
-      let point = [x_offset + x as f64, y as f64, y_offset + z as f64].map(|x| x * TERRAIN_H_SCALE);
-      let h = MIN_TERRAIN_HEIGHT + (fbm.get(point) * TERRAIN_HEIGHT).round() as usize;
+      let point = [x_offset + x as f64, y_offset + z as f64].map(|x| x * TERRAIN_H_SCALE);
+      let h = MIN_TERRAIN_HEIGHT + (TERRAIN_HEIGHT_HALF + (fbm.get(point) * TERRAIN_HEIGHT_HALF).round()) as usize - MIN_TERRAIN_HEIGHT;
       for y in 0..h {
         blocks[x][y][z] = Block{ block_type: 1 };
       }
