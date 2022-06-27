be2d696ca66afb71d756bf687c19e035fe273bf7
griffi-gh <prasol258@gmail.com>
2022-06-27 21:16:17 +0200

    .

--- shared/src/types.rs
+++ shared/src/types.rs
@@ -1,4 +1,4 @@
-use crate::consts::{CHUNK_HEIGHT, CHUNK_SIZE, PROTOCOL_ID};
+use crate::consts::{CHUNK_HEIGHT, CHUNK_SIZE};
 use serde::{Serialize, Deserialize};
 use serde_with::serde_as;
 use compress::rle;
