feadd2070cf37168703eb92171fc4c259963c49b
griffi-gh <prasol258@gmail.com>
2022-07-03 19:03:02 +0200

    menu

--- client/src/main_menu.rs
+++ client/src/main_menu.rs
@@ -1,26 +1,61 @@
 use bevy::prelude::*;
 use iyes_loopless::prelude::*;
 
-use bevy_egui::{egui, EguiContext};
+use bevy_egui::{egui::{self, Align2, Vec2 as EVec2}, EguiContext};
 use crate::GameState;
 
+#[derive(Default, PartialEq)]
+#[non_exhaustive]
+enum MainMenuScreen {
+  #[default]
+  Main,
+  Connect
+}
+
 #[derive(Default)]
-struct MainMenuGuiState {}
+struct MainMenuGuiState {
+  screen: MainMenuScreen
+}
 
 fn main_menu_gui(
   mut commands: Commands,
   mut egui_context: ResMut<EguiContext>,
-  //mut gui_state: ResMut<MainMenuGuiState>,
+  mut gui_state: ResMut<MainMenuGuiState>,
   mut exit: EventWriter<bevy::app::AppExit>
 ) {
-  egui::Window::new("Main menu").show(egui_context.ctx_mut(), |ui| {
-    if ui.button("Play").clicked() {
-      commands.insert_resource(NextState(GameState::Connecting));
-    }
-    if ui.button("Exit").clicked() {
-      exit.send_default();
-    }
-  });
+  egui::Window::new("Main menu")
+    .collapsible(false)
+    .default_width(300.)
+    .anchor(Align2::CENTER_CENTER, EVec2::ZERO)
+    .show(egui_context.ctx_mut(), |ui| {
+      ui.vertical_centered_justified(|ui| {
+
+        //Stuff
+        match gui_state.screen {
+          MainMenuScreen::Main => {
+            if ui.button("Play").clicked() {
+              gui_state.screen = MainMenuScreen::Connect;
+            }
+            if ui.button("Exit").clicked() {
+              exit.send_default();
+            }
+          },
+          MainMenuScreen::Connect => {
+            if ui.button("[DEBUG]\nConnect to localhost").clicked() {
+              commands.insert_resource(NextState(GameState::Connecting));
+            }
+          }
+        }
+
+        //Back button
+        if gui_state.screen != MainMenuScreen::Main {
+          if ui.button("<= Back").clicked() {
+            gui_state.screen = MainMenuScreen::Main;
+          }
+        }
+
+      });
+    });
 }
 
 pub struct MainMenuPlugin;
